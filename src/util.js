73391dea57c91c824b0a3261a8911be74d94dada
ekelen <ekelen@users.noreply.github.com>
2021-01-30 19:26:51 +0000

    deploy: e698e600549de2967ed6e49e6a6a2e7493936cb7

--- src/util.js
+++
@@ -1,7 +0,0 @@
-const now = new Date();
-const year = now.getFullYear();
-const dateStartValue = new Date(year, now.getMonth(), now.getDate(), 0, 0, 0);
-module.exports.dateString = `${dateStartValue}`.slice(
-  0,
-  `${dateStartValue}`.indexOf(`${year}`) + `${year}`.length
-);

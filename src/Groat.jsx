477e670ff286a321aa81aeab007ad8a90ce34a9e
Elizabeth Kelen <erskelen@gmail.com>
2021-03-08 13:42:46 -0500

    remove debug, edges working

--- src/Groat.jsx
+++ src/Groat.jsx
@@ -3,28 +3,28 @@ import React from "react";
 const Groat = ({ groatData }) => {
   const { name, region, date, source, imgSrcObv, imgSrcRev } = groatData;
   return (
-    <div className="dynamic-content">
-      <div className="content-main">
-        <div className="content-box">
-          <div className="groat-images">
-            {imgSrcObv && <img src={imgSrcObv} alt={imgSrcObv} />}
-            {imgSrcRev && <img src={imgSrcRev} alt={imgSrcObv} />}
-          </div>
-          <div className="groat-text">
-            <p>
-              You are given one <strong>{name + " "}</strong>
-              in the region of <strong>{region}</strong> sometime between the
-              years of <strong>{date}</strong>.
-            </p>
-            <p>
-              <a href={source} alt={`Source: ${source}`} className="external">
-                Source
-              </a>
-            </p>
-          </div>
+    // <div className="dynamic-content">
+    <div className="content-main">
+      <div className="content-box">
+        <div className="groat-images">
+          {imgSrcObv && <img src={imgSrcObv} alt={imgSrcObv} />}
+          {imgSrcRev && <img src={imgSrcRev} alt={imgSrcObv} />}
+        </div>
+        <div className="groat-text">
+          <p>
+            You are given one <strong>{name + " "}</strong>
+            in the region of <strong>{region}</strong> sometime between the
+            years of <strong>{date}</strong>.
+          </p>
+          <p>
+            <a href={source} alt={`Source: ${source}`} className="external">
+              Source
+            </a>
+          </p>
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 

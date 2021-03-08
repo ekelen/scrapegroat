200abca308a816c87c1996280fa308de33165e3e
Elizabeth Kelen <erskelen@gmail.com>
2021-03-08 12:39:44 -0500

    copypaste at first

--- src/Groat.jsx
+++ src/Groat.jsx
@@ -4,21 +4,25 @@ const Groat = ({ groatData }) => {
   const { name, region, date, source, imgSrcObv, imgSrcRev } = groatData;
   return (
     <div className="dynamic-content">
-      <div className="groat-images">
-        {imgSrcObv && <img src={imgSrcObv} alt={imgSrcObv} />}
-        {imgSrcRev && <img src={imgSrcRev} alt={imgSrcObv} />}
-      </div>
-      <div className="groat-text">
-        <p>
-          You are given one <strong>{name + " "}</strong>
-          in the region of <strong>{region}</strong> sometime between the years
-          of <strong>{date}</strong>.
-        </p>
-        <p>
-          <a href={source} alt={`Source: ${source}`} className="external">
-            Source
-          </a>
-        </p>
+      <div className="content-main">
+        <div className="content-box">
+          <div className="groat-images">
+            {imgSrcObv && <img src={imgSrcObv} alt={imgSrcObv} />}
+            {imgSrcRev && <img src={imgSrcRev} alt={imgSrcObv} />}
+          </div>
+          <div className="groat-text">
+            <p>
+              You are given one <strong>{name + " "}</strong>
+              in the region of <strong>{region}</strong> sometime between the
+              years of <strong>{date}</strong>.
+            </p>
+            <p>
+              <a href={source} alt={`Source: ${source}`} className="external">
+                Source
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
